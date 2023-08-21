cf91ba1e25640f5402a79f5ffcb5d773c61aa5e8
jason <591831416@qq.com>
2023-08-21 11:36:28 +0800

    update-20230821

---
+++ src/main/java/com/bupt/ArtWorkApplication.java
@@ -0,0 +1,16 @@
+package com.bupt;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.csc.modules.ums.mapper")
+public class ArtWorkApplication {
+
+    public static void main(String[] args) {
+        System.out.println("just a test");
+        SpringApplication.run(ArtWorkApplication.class, args);
+    }
+
+}
