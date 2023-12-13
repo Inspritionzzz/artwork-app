5f887e8f1c570e404d72fe682d1133840af0340b
jason <591831416@qq.com>
2023-12-13 08:52:25 +0800

    update-20231213

--- src/main/java/com/bupt/config/RedisConfig.java
+++ src/main/java/com/bupt/config/RedisConfig.java
@@ -8,9 +8,9 @@ import org.springframework.context.annotation.Configuration;
  * Redis配置类
  * Created by macro on 2020/3/2.
  */
-@EnableCaching
-@Configuration
-public class RedisConfig extends BaseRedisConfig {
-
-}
-
+//@EnableCaching
+//@Configuration
+//public class RedisConfig extends BaseRedisConfig {
+//
+//}
+//
